e392973b267ee5193f700c91a55eae45c3ddc9cd
MOONCOM <xuyong199312@sina.com>
2021-01-01 20:28:00 +0800

    1、修改了在线演示页面的代码

--- src/demo/main.js
+++ src/demo/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import DemoComponent from './demo.vue';
 import highlight from "../index";
 
-Vue.use(highlight,{class:'MOONCOM zhangsan',style:{color:'#0ff'}});
+Vue.use(highlight,{class:'MOONCOM zhangsan',style:{color:'#333'},selectedClass: 'highlight'});
 new Vue({
   render: h => h(DemoComponent),
 }).$mount('#app');
